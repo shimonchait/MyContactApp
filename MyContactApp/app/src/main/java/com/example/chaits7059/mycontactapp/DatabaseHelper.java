c824b2d8d8453ebc7aaccead56e24d7250d19c22
chaits7059 <shimonchait@gmail.com>
2017-05-12 09:23:09 -0700

    Shimon Chait: Updated DatabaseHelper and MainActivity to create Database
    Added Name field and 'add' button

---
+++ MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.example.chaits7059.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by chaits7059 on 5/12/2017.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Contact.db";
+    public static final String TABLE_NAME = "contact_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+    public DatabaseHelper(Context context ){
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
