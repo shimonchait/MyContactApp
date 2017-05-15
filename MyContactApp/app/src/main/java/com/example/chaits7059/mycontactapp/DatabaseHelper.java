507760a8999541e2296b2ae95099bcfefcfd06d3
chaits7059 <shimonchait@gmail.com>
2017-05-15 09:29:03 -0700

    ADD DATA button working for editName Text Field
    Changed the display, created a boolean testing if insertData works,
    figure out how to make insertData work for the editName text field.

--- MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.chaits7059.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,6 +14,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+
 
     public DatabaseHelper(Context context ){
         super(context, DATABASE_NAME, null, 1);
@@ -28,5 +30,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+
+    public boolean insertData (String name){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
+
+    }
+
 
 }
--- MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.chaits7059.mycontactapp;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,8 +24,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
     }
 
+    public void addData(View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+        if (isInserted == true) {
+            Log.d("MyContactt", "Data insertion successful");
+            //Create toast message to user indicating data inserted correctly.
+        }
 
+        else {
+            Log.d("MyContactt", "Data insertion NOT successful");
+            //Create toast message to User indicating data inserted incorrectly.
+        }
+
+    }
 
 }
