3dd1e7eebc38242b2d6b575927cae407f9518660
chaits7059 <shimonchait@gmail.com>
2017-05-16 09:30:36 -0700

    Failed to make AddData Work
    Added preliminary code for View Contact button and working on making
    addData work

--- MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/DatabaseHelper.java
@@ -2,8 +2,10 @@ package com.example.chaits7059.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by chaits7059 on 5/12/2017.
@@ -14,6 +16,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
 
 
     public DatabaseHelper(Context context ){
@@ -22,7 +26,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT. ADDRESS TEXT)");
     }
 
     @Override
@@ -31,17 +35,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData (String name){
+    public boolean insertData (String name, String age, String address){
 
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, name);
 
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, age);
+        contentValues.put(COL_4, address);
         long result = db.insert(TABLE_NAME, null, contentValues);
+
+
+
         if(result == -1) return false;
         else return true;
 
     }
 
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
 
 }
--- MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/chaits7059/mycontactapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chaits7059.mycontactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +10,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
 
 
@@ -26,19 +30,49 @@ public class MainActivity extends AppCompatActivity {
         myDb = new DatabaseHelper(this);
 
         editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
     }
 
     public void addData(View v) {
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editAddress.getText().toString());
         if (isInserted == true) {
             Log.d("MyContactt", "Data insertion successful");
-            //Create toast message to user indicating data inserted correctly.
+            Toast.makeText(MainActivity.this, "SUCCESS: Your Data Has Been Inserted",
+                    Toast.LENGTH_LONG).show();
         }
 
         else {
             Log.d("MyContactt", "Data insertion NOT successful");
-            //Create toast message to User indicating data inserted incorrectly.
+            Toast.makeText(MainActivity.this, "FAILURE: Your Data Has Not Been Inserted",
+                    Toast.LENGTH_LONG).show();
         }
+
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            Log.d("MyContactt", "Error: No data found in database");
+            Toast.makeText(MainActivity.this, "ERROR: No Data found in database",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+
+
+        //set up a loop with Cursor moveToNext Method
+        //    append each COL to buffer
+        //use the getString method
+
+        showMessage("Data", buffer.toString());
+
+
+    }
+
+    private void showMessage(String error, String s) {
 
     }
 
